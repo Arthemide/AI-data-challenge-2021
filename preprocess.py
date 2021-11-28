5d3d734e46289ac5073780c04e53a343392ae55e
Gireg Roussel <giregroussel@free.fr>
2021-11-28 16:49:11 +0100

    clean code and update readme

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,6 @@
-#feature_cols = []
+import numpy as np
+from datetime import datetime
+
 stade_cols = [
     'Zone anterieure droite bas',
     'Zone anterieure droite haut',
@@ -39,8 +41,6 @@ int_cols = [
 ]
 
 unique_cols = [
-    # 'Nom du centre',
-    # "id",
     'age',
     'Sexe',
     'Lieu de provenance du patient',
@@ -52,42 +52,42 @@ unique_cols = [
 feature_cols = stade_cols + boolean_cols + unique_cols + int_cols
 
 def dt64_to_float(dt64):
-    if type(dt64) is float and np.isnan(dt64):
-        return .0 # np.nan
-    try:
-        time = datetime.strptime(dt64, '%Y-%m-%d')
-        return time.timestamp()
-    except Exception as e:
-        print(dt64)
-        print(e)
-    return dt64
+  if type(dt64) is float and np.isnan(dt64):
+    return .0 # np.nan
+  try:
+    time = datetime.strptime(dt64, '%Y-%m-%d')
+    return time.timestamp()
+  except Exception as e:
+    print(dt64)
+    print(e)
+  return dt64
 
 def convert(x):
-    if x == 'Oui':
-        return 1
-    return 0
+  if x == 'Oui':
+    return 1
+  return 0
 
 def pre_processing(df):
-    dataFrame = df.copy()
-    # Change date from object to float
-    dataFrame['Date de debut de la symptomatologie'] = dataFrame['Date de debut de la symptomatologie'].map(dt64_to_float).to_numpy()
+  dataFrame = df.copy()
+  # Change date from object to float
+  dataFrame['Date de debut de la symptomatologie'] = dataFrame['Date de debut de la symptomatologie'].map(dt64_to_float).to_numpy()
 
-    # Change option to int
-    dataFrame['Oxygenotherapie'] = dataFrame['Oxygenotherapie'].map({"Air ambiant": 1, "Moderee": 2, "Assistance respiratoire": 3, np.nan:0}).to_numpy()
+  # Change option to int
+  dataFrame['Oxygenotherapie'] = dataFrame['Oxygenotherapie'].map({"Air ambiant": 1, "Moderee": 2, "Assistance respiratoire": 3, np.nan:0}).to_numpy()
 
-    # Change string to int
-    dataFrame['Sexe'] = dataFrame['Sexe'].map({"Masculin": 0, "Feminin": 1}).to_numpy()
+  # Change string to int
+  dataFrame['Sexe'] = dataFrame['Sexe'].map({"Masculin": 0, "Feminin": 1}).to_numpy()
 
-    # Change string to int
-    dataFrame['Lieu de provenance du patient'] = dataFrame['Lieu de provenance du patient'].map({"Domicile": 0, "EHPAD": 1, "Hopital": 2, "Autre": 3, np.nan:4}).to_numpy()
+  # Change string to int
+  dataFrame['Lieu de provenance du patient'] = dataFrame['Lieu de provenance du patient'].map({"Domicile": 0, "EHPAD": 1, "Hopital": 2, "Autre": 3, np.nan:4}).to_numpy()
 
-    # Change option to int
-    dataFrame['Echographiste'] = dataFrame['Echographiste'].map({"Forme pour l'epidemie": 1, "Experience d'echographie": 2, "Expert": 3, np.nan:0}).to_numpy()
+  # Change option to int
+  dataFrame['Echographiste'] = dataFrame['Echographiste'].map({"Forme pour l'epidemie": 1, "Experience d'echographie": 2, "Expert": 3, np.nan:0})
 
 
-    dataFrame[boolean_cols] = dataFrame[boolean_cols].apply(lambda value: value.map(lambda x: 1 if x == 'Oui' else 0))
+  dataFrame[boolean_cols] = dataFrame[boolean_cols].apply(lambda value: value.map(lambda x: 1 if x == 'Oui' else 0))
 
-    # Change data from string "Stade X" to int X
-    dataFrame[stade_cols] = dataFrame[stade_cols].apply(lambda value: value.map(lambda x: int(x[-1]) if type(x) is str else x))
+  # Change data from string "Stade X" to int X
+  dataFrame[stade_cols] = dataFrame[stade_cols].apply(lambda value: value.map(lambda x: int(x[-1]) if type(x) is str else x))
 
-    return dataFrame
+  return dataFrame
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from sklearn.pipeline import FeatureUnion, make_pipeline
 from sklearn.impute import SimpleImputer, MissingIndicator
-from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, roc_auc_score
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV, KFold
 
 def get_transformer():
@@ -13,7 +13,7 @@ def get_transformer():
 def get_pipeline(model):
     return make_pipeline(get_transformer(), model)
 
-def fit(clf, params, cv=10, X_train=X_train, y_train=y_train):
+def fit(clf, params, X_train, y_train, cv=10):
     grid = GridSearchCV(
         clf, 
         params, 
@@ -28,14 +28,9 @@ def fit(clf, params, cv=10, X_train=X_train, y_train=y_train):
     return grid
 
 def best_scores(model):
-    # print(f'The mean cross validation test score is: {model.cv_results_.mean_test_score}') #for some reason this wasn't working for me even though the attribute exists so lets just leave it.
     print(f'The best parameters are: {model.best_params_}')
     print(f'The best score that we got is: {model.best_score_}')
     return None
 
 def check_scores(y_test, y_pred):
-    # print('Precision: %.3f' % precision_score(y_test, y_pred))
-    # print('Recall: %.3f' % recall_score(y_test, y_pred))
     print('Accuracy: %.4f' % accuracy_score(y_test, y_pred))
-    # print('F1 Score: %.3f' % f1_score(y_test, y_pred))
-    # print('ROC-AUC Score: %.3f' % roc_auc_score(y_test, y_pred))
